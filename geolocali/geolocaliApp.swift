0e172c208eba8bfcedfee3c2128ab837318aeeac
alessia <cuoringiro@gmail.com>
2024-12-10 19:24:24 +0100

    Initial Commit

---
+++ geolocali/geolocaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  geolocaliApp.swift
+//  geolocali
+//
+//  Created by alessia frezzetti on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct geolocaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
